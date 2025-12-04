50331a7b541fc8624764a385f4a232065d22f176
SangeethKumarPG <pgsangeethkumar@gmail.com>
2025-12-04 12:49:00 +0530

    adding cors

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const authRoutes = require('./routes/authRoutes');
 const siteRoutes = require('./routes/siteRoutes');
 const imageRoutes = require('./routes/imageRoutes');
 
 const app = express();
+
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static('uploads'));
